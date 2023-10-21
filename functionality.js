5bca4e7827de8348ea814fd167870f9d79455fc0
Augustine Fuertes <141537663+MeadowOwner@users.noreply.github.com>
2023-10-21 16:28:24 -0700

    Added functionality to the submit button. Submit puts the text in the form, within the letter and postcard.

--- functionality.js
+++ functionality.js
@@ -45,5 +45,50 @@ envButt.addEventListener('click', clickEnv);
 postButt.addEventListener('click', clickPost);
 bothButt.addEventListener('click', clickBoth);
 
+// textboxes in the envelope and the postcard
+const rNameBox = document.querySelectorAll('.receiver .fName');
+const rAddrBox = document.querySelectorAll('.receiver .stAddr');
+const rElse = document.querySelectorAll('.receiver .ciStZi');
+const rCountry = document.querySelectorAll('.receiver .country');
+
+const sNameBox = document.querySelectorAll('.sender .fName');
+const sAddrBox = document.querySelectorAll('.sender .stAddr');
+const sElse = document.querySelectorAll('.sender .ciStZi');
+const sCountry = document.querySelectorAll('.sender .country');
+
+
+const form = document.getElementById('info');
+
+form.addEventListener('submit', (event) =>{
+    // putting the name in the envelope and postcard
+
+    // receiver portion
+    event.preventDefault();
+    for(curr of rNameBox){
+        curr.textContent = form.elements['rName'].value;
+    }
+
+    for(curr of rCountry){
+        curr.textContent = form.elements['rCountry'].value;
+    }
+
+    for(curr of rElse){
+        let comma = ', ';
+        if(form.elements['rCity'].value == '' || form.elements['rState'].value == ''){
+            comma = ' ';
+        }
+        curr.textContent = form.elements['rCity'].value + comma + form.elements['rState'].value + ' ' +
+            form.elements['rZip'].value;
+    }
+
+    for(curr of rAddrBox){
+        curr.textContent = form.elements['rStrAddress'].value;
+        if(form.elements['rAppartment'].value != ''){
+            curr.textContent += ', ' + form.elements['rAppartment'].value; 
+        }
+    }
+
+    //sender portion
+});
 
 
